9d25a2597238bdc03828d4912601c29132617861
Masum Modi <masummodi1994@gmail.com>
2021-04-26 13:37:05 -0400

    feat: field injection in activity

--- Demo Projects/Dagger 2/app/src/main/java/com/mm/daggerapplication/MainActivity.kt
+++ Demo Projects/Dagger 2/app/src/main/java/com/mm/daggerapplication/MainActivity.kt
@@ -5,16 +5,23 @@ import android.os.Bundle
 import com.mm.daggerapplication.daggerComponents.CarComponent
 import com.mm.daggerapplication.daggerComponents.DaggerCarComponent
 import com.mm.daggerapplication.models.Car
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject
     lateinit var car: Car
+
+    @Inject
+    lateinit var car1: Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        car = DaggerCarComponent.create().getCar()
+        val carComponent = DaggerCarComponent.create()
+        carComponent.inject(this)
         car.drive()
+        car1.drive()
     }
 }
--- Demo Projects/Dagger 2/app/src/main/java/com/mm/daggerapplication/daggerComponents/CarComponent.kt
+++ Demo Projects/Dagger 2/app/src/main/java/com/mm/daggerapplication/daggerComponents/CarComponent.kt
@@ -1,9 +1,13 @@
 package com.mm.daggerapplication.daggerComponents
 
+import android.app.Activity
+import com.mm.daggerapplication.MainActivity
 import com.mm.daggerapplication.models.Car
 import dagger.Component
 
 @Component
 interface CarComponent {
     fun getCar(): Car
+
+    fun inject(mainActivity: MainActivity)
 }
