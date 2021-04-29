0b180def622f4ef2c676b08308af06aa2e602579
Masum Modi <masummodi1994@gmail.com>
2021-04-29 14:39:22 -0400

    feat: Kodein framework for dependency injection

---
+++ Demo Projects/MVVM Coroutines/app/src/main/java/com/example/mvvmsample/MyApp.kt
@@ -0,0 +1,31 @@
+package com.example.mvvmsample
+
+import android.app.Application
+import com.example.mvvmsample.data.db.AppDatabase
+import com.example.mvvmsample.data.network.MyApi
+import com.example.mvvmsample.data.network.NetworkConnectionInterceptor
+import com.example.mvvmsample.data.repositories.UserRepository
+import com.example.mvvmsample.ui.auth.AuthViewModel
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class MyApp : Application(), KodeinAware {
+
+    override val kodein = Kodein.lazy {
+
+        import(androidXModule(this@MyApp))
+
+        bind() from singleton { NetworkConnectionInterceptor(instance()) }
+        bind() from singleton { MyApi(instance()) }
+        bind() from singleton { AppDatabase(instance()) }
+        bind() from singleton { UserRepository(instance(), instance()) }
+        bind() from provider { AuthViewModel(instance()) }
+
+    }
+
+}
--- Demo Projects/MVVM Coroutines/app/src/main/java/com/example/mvvmsample/ui/auth/LoginActivity.kt
+++ Demo Projects/MVVM Coroutines/app/src/main/java/com/example/mvvmsample/ui/auth/LoginActivity.kt
@@ -6,29 +6,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.mvvmsample.R
-import com.example.mvvmsample.data.db.AppDatabase
 import com.example.mvvmsample.data.db.entities.User
-import com.example.mvvmsample.data.network.MyApi
-import com.example.mvvmsample.data.network.NetworkConnectionInterceptor
 import com.example.mvvmsample.data.repositories.UserRepository
 import com.example.mvvmsample.databinding.ActivityLoginBinding
 import com.example.mvvmsample.ui.home.HomeActivity
 import com.example.mvvmsample.util.hide
 import com.example.mvvmsample.util.show
 import com.example.mvvmsample.util.snackbar
-import kotlinx.android.synthetic.main.activity_login.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class LoginActivity : AppCompatActivity(), AuthListener {
+class LoginActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein by kodein()
+
+    private val repository: UserRepository by instance()
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val networkConnectionInterceptor = NetworkConnectionInterceptor(this)
-        val api = MyApi(networkConnectionInterceptor)
-        val db = AppDatabase(this)
-        val repository = UserRepository(api,db)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
-        val binding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         val viewModel = AuthViewModel(repository)
+
         binding.viewModel = viewModel
         viewModel.authListener = this
 
@@ -43,15 +45,16 @@ class LoginActivity : AppCompatActivity(), AuthListener {
     }
 
     override fun onStarted() {
-        progress_bar.show()
+        binding.progressBar.show()
     }
 
     override fun onSuccess(user: User) {
-        progress_bar.hide()
+        binding.progressBar.hide()
     }
 
     override fun onFailure(message: String) {
-        progress_bar.hide()
-        root_layout.snackbar(message)
+        binding.progressBar.hide()
+        binding.rootLayout.snackbar(message)
     }
+
 }
